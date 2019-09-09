88ffd12cfdb4db2675962fc60b32c8e20cfae415
pedrovmc6 <41556042+pedrovmc6@users.noreply.github.com>
2019-09-09 10:45:22 -0300

    Update batalha.cpp

--- OBI/2018/batalha.cpp
+++ OBI/2018/batalha.cpp
@@ -3,27 +3,27 @@
 using namespace std;
 
 struct status{
-	int F,D;
+	int F,D; //Força,Dano;
 };
 
 status j,k;
 
-int x,y;
+int x , y;
 
 int main(){
 
-	cin>>j.F>>j.D;
-	cin>>k.F>>k.D;
+	cin >> j.F >> j.D;
+	cin >> k.F >> k.D;
 
-	x=j.D-k.F;
-	y=k.D-j.F;
+	x = j.D - k.F;
+	y = k.D - j.F;
 
 	if( (x == 0 and y == 0) or (x != 0 and y != 0) ){
-		cout<<"-1\n";
+		cout << "-1\n";
 	}else if( x == 0 and y != 0){
-		cout<<"1\n";
+		cout << "1\n";
 	}else if( x != 0 and y == 0){
-		cout<<"2\n";
+		cout<< "2\n";
 	}
 
 
